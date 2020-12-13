5a0db6bb09f26a77f9ad4f82d2baf1079abf50d0
amiratashani <amiratashanii@gmail.com>
2020-12-13 23:24:11 +0330

    add third party radium for hover button in inline style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
+import Radium from "radium";
 import Person from './Person/Person';
 
 
@@ -53,7 +54,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     }
 
     let persons = null
@@ -73,6 +78,10 @@ class App extends Component {
       )
 
       style.backgroundColor = "red"
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      }
     }
 
     const classes = []
@@ -100,4 +109,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default Radium(App);
