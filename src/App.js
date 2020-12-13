6fcf82076dbcf59a0b2aef05b8edc38986bbf39f
amiratashani <amiratashanii@gmail.com>
2020-12-13 14:40:07 +0330

    add key to person list componet

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 }
+      { id: "abcd1", name: "Max", age: 28 },
+      { id: "efgh2", name: "Manu", age: 29 },
+      { id: "higk3", name: "Stephanie", age: 26 }
     ],
     otherState: "some other value",
     showPersons: false
@@ -58,7 +58,8 @@ class App extends Component {
             return (<Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />)
+              age={person.age}
+              key={person.id} />)
           })}
         </div>
       )
