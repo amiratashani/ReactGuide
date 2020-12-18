22428c5386f33fda6ed0c8f8dd91921fe744ea4b
amiratashani <amiratashanii@gmail.com>
2020-12-18 15:07:44 +0330

    Controlling the useEffect() Behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,7 +6,10 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log("[Cockpit.ja useEffect]")
         // Http request...
-    })
+        setTimeout(()=>{
+            alert("Saved data to cloud!")
+        },1000)
+    },[props.persons])
 
 
     const assignClasses = []
