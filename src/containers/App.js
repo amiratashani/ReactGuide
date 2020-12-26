1755ae893b7968dc0ed3d23284d86d004a0e10d3
amiratashani <amiratashanii@gmail.com>
2020-12-26 14:55:37 +0330

    Another Form of HOCs

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
-import WithClass from '../hoc/WithClass'
+import withClass from '../hoc/withClass'
+import Aux from '../hoc/Auxiliary'
 
 import ErrorBoundry from '../ErrorBoundary/ErrorBoundary';
 
@@ -110,7 +111,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {
           this.setState({ showCockpit: false })
         }}>
@@ -122,11 +123,11 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         /> : null}
         {persons}
-      </WithClass>
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 
 }
 
-export default App;
+export default withClass(App,classes.App);
