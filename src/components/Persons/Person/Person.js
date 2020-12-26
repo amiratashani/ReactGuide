9849c530117a8dc3ad75a34e7243af60823d4618
amiratashani <amiratashanii@gmail.com>
2020-12-26 13:23:30 +0330

    use React.Fragment instead of Aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
+
+import Aux from '../../../hoc/Auxiliary'
 import classes from './Person.module.css'
 
 
@@ -14,15 +16,13 @@ class Person extends Component {
         console.log("[Person.js rendering...]")
 
         return (
-
-            // <div className="Person" style={style} >
-            [
-                < p key="i1" onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>,
-                < p key="i2"> {this.props.children}</p >,
+            <Fragment>
+                < p key="i1" onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
+                < p key="i2"> {this.props.children}</p >
                 <input key="i3" type="text"
                     onChange={this.props.changed}
                     value={this.props.name} />
-            ]
+           </Fragment>
         )
     }
 
